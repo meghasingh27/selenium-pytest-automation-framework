4576c4209bb6bbec3221a81e1ba8ca46c43107a0
Megha Singh <singhmegha2700@gmail.com>
2026-06-06 15:23:12 +0530

    Implemented login test with page object model

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,13 @@
-def test_launch_application(driver):
+from pages.login_page import LoginPage
 
-    assert "Swag Labs" in driver.title
+
+def test_valid_login(driver):
+
+    login_page = LoginPage(driver)
+
+    login_page.login(
+        "standard_user",
+        "secret_sauce"
+    )
+
+    assert "inventory" in driver.current_url
